f9248c1e43036403bf600679aa41f513af1845a2
Macko++ <82177845+Macko-pp@users.noreply.github.com>
2024-01-11 14:04:38 -0500

    Finished triangle exercise

--- triangle.py
+++ triangle.py
@@ -6,7 +6,15 @@ square_side1 = side1 ** 2
 square_side2 = side2 ** 2
 square_side3 = side3 ** 2
 
-if (side1 + side2 <= side3 and side1 + side3 <= side2 and side2 + side3 <= side1):
+print("-------------------------------------------------------------------------------\n")
+
+print("Report:")
+print(f"Side 1: {side1}")
+print(f"Side 2: {side2}")
+print(f"Side 3: {side3}")
+
+if (side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1):
+    print("Triangle Exists")
 
     if (side1 == side2 == side3):
         print("Triangle is equilateral")
@@ -15,7 +23,7 @@ if (side1 + side2 <= side3 and side1 + side3 <= side2 and side2 + side3 <= side1
         print("Triangle is isosceles")
 
     else:
-        print("Triangle is obtuse")
+        print("Triangle is scalene")
 
 else:
     print("Triangle does not exist")
